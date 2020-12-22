b098c6346b31fb802aca0d678b7702ed49f50315
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-12-22 23:21:07 +0530

    detecting device orientation

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useDeviceOrientation } from "@react-native-community/hooks";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
@@ -6,20 +7,42 @@ import CustomButton from "./components/CustomButton";
 import CustomImage from "./components/CustomImage";
 import { ImageSizeProvider } from "./contexts/ImageSize";
 
+const View1 = () => (
+  <>
+    <Text style={styles.text}>
+      This should be able to start the project on phone
+    </Text>
+    {/* add the height and the width to the source objcect and not as props */}
+    <CustomImage />
+    <CustomAlert />
+    <CustomButton />
+  </>
+);
+
+const View2 = () => {
+  const { landscape, portrait } = useDeviceOrientation();
+  return (
+    <>
+      <View style={styles.container2}>
+        <Text>view is like a div</Text>
+        <Text>occupies 50% of both height and width</Text>
+        <Text>You are in {landscape ? "landscape" : "portrait"} mode</Text>
+      </View>
+    </>
+  );
+};
+
 export default function App() {
   return (
-    <ImageSizeProvider>
-      <View style={styles.container}>
-        <Text style={styles.text}>
-          This should be able to start the project on phone
-        </Text>
-        {/* add the height and the width to the source objcect and not as props */}
-        <CustomImage />
-        <StatusBar style="auto" />
-        <CustomButton />
-      </View>
-      <CustomAlert />
-    </ImageSizeProvider>
+    <>
+      <ImageSizeProvider>
+        <View style={styles.container}>
+          {/* <View1 /> */}
+          <View2 />
+        </View>
+      </ImageSizeProvider>
+      <StatusBar style="auto" />
+    </>
   );
 }
 
@@ -34,5 +57,10 @@ const styles = StyleSheet.create({
     fontSize: 30,
     textAlign: "center",
     color: "green",
+  },
+  container2: {
+    backgroundColor: "#abc",
+    height: "50%",
+    width: "50%",
   },
 });
