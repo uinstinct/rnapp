2bda74166da247238cfa3437956661d78cdb666d
aditya-mitra <ultrapokeinstinct@gmail.com>
2020-12-22 18:50:16 +0530

    safe view, numberOfLines and onPress

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const handlePress = ()=>{
+    console.log('mobile is always changing');
+  }
+  
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>This should be able to start the project on phone</Text>
       <Text>Check if refreshing works</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        This is a very long text. This will show ellipsis when out of the area because number of lines set is 1
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
